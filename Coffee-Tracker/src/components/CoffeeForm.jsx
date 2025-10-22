d095fd1f76658ff5be7bfc1c9093bc62341e47d0
Coffee-Tracker <uranusfor@gmail.com>
2025-10-22 12:57:08 +0530

    added state to the other button

--- Coffee-Tracker/src/components/CoffeeForm.jsx
+++ Coffee-Tracker/src/components/CoffeeForm.jsx
@@ -1,6 +1,10 @@
 import { coffeeOptions } from '../utils'
+import { useState } from 'react'
 
 export default function CoffeeForm() {
+  const [showCoffeeType, setShowCoffeeType] = useState(false)
+
+  
   return (
     <>
       <div className="section-header">
@@ -19,7 +23,7 @@ export default function CoffeeForm() {
             )
           })
         }
-        <button className='button-card'>
+        <button onClick={() => setShowCoffeeType(true)} className='button-card'>
           <h4>
             Other
           </h4>
@@ -31,17 +35,43 @@ export default function CoffeeForm() {
       <select id='coffee-list' name='coffee-list'>
         <option value={null}>Select type</option>
         {coffeeOptions.map((option, optionIndex) => {
-          return(
+          return (
             <option key={optionIndex} value={option.name}>
               {option.name} ({option.caffeine}mg)
-              </option>
+            </option>
 
           )
-})}
+        })}
 
       </select>
       <h4>Add the cost ($)</h4>
-      <input ></input>
+      <input className='w-full' type='number' placeholder='4.5' />
+      <h4>Time since consumption</h4>
+      <div className='time-entry'>
+        <div>
+          <h6>
+            Hours
+          </h6>
+          <select id='hours-select'>
+            {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23].map((hour, hourIndex) => {
+              return <option key={hourIndex} value={hour}>{hour}</option>
+            })}
+          </select>
+        </div>
+        <div>
+          <h6>
+            Mins
+          </h6>
+          <select id='mins-select'>
+            {[0, 5, 10, 15, 20, 25, 30, 35, 40, 45].map((min, minIndex) => {
+              return <option key={minIndex} value={min}>{min}</option>
+            })}
+          </select>
+        </div>
+      </div>
+      <button>
+        <p>Add Entry</p>
+      </button>
     </>
   )
 }
