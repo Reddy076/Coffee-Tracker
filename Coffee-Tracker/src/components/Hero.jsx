06805b4b25b32d440beaef9a9a18f13efcee24f3
Coffee-Tracker <uranusfor@gmail.com>
2025-10-22 11:33:44 +0530

    completed the hero component

--- Coffee-Tracker/src/components/Hero.jsx
+++ Coffee-Tracker/src/components/Hero.jsx
@@ -3,6 +3,34 @@ import React from 'react'
 export default function Hero() {
   return (
     <>
+      <h1>
+        Coffee Tracking for Coffee
+         <abbr title='An enthusiast or devotee'> Fiends</abbr>!
+      </h1>
+      <div className='benefits-list' >
+        <h3 className='font-bolder'>Try <span className='text-gradient'>Caffiend</span> and start ...</h3>
+        <p>
+          ✅ Tracking every coffee
+        </p>
+        <p>
+          ✅ Measuring your blood caffeine levels
+        </p>
+        <p>
+          ✅ costing and quantifying your caffeine addition
+        </p>
+      </div>
+      <div className='card info-card'>
+        <div>
+          <i className="fa-solid fa-circle-info"></i>
+          <h3>
+            Did you know...
+          </h3>
+        </div>
+        <h5>That Caffeine&apos;s half-life is about 5 hours?</h5>
+        <p>
+          this means that after 5 hours, half the caffeine you consumed is still in your system, keeping you alert longer! so if you drink a cup of coffee with 200 mg of coffee, 5 hours later, you&apos;ll still have 100mg of caffeine in your system.
+        </p>
+      </div>
     </>
   )
 }
--- Coffee-Tracker/src/components/Layout.jsx
+++ Coffee-Tracker/src/components/Layout.jsx
@@ -3,12 +3,27 @@ import React from 'react'
 export default function Layout({ children }) {
   const header=(
     <header>
-
+      <div>
+        <h1 className='text-gradient'>
+          CAFFIEND
+        </h1>
+        <p>
+          For coffee Insatiates
+        </p>
+      </div>
+      <button>
+        <p>
+          Sign Up free
+        </p>
+        <i className="fa-solid fa-mug-hot"></i>
+      </button>
     </header>)
   
   const footer=(
     <footer>
-    
+      <p>
+        <span className='text-gradient'>Caffiend</span>was made by <a href="https://github.com/Dasthagiri">Dasthagiri</a>
+      </p>
     </footer>
   )
   
