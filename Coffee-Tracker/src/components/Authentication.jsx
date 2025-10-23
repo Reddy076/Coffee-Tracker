90f753b59a84c1e90dd7df633ee5e311169d6798
Coffee-Tracker <uranusfor@gmail.com>
2025-10-23 16:51:25 +0530

    added fire base authentication to the application

--- Coffee-Tracker/src/components/Authentication.jsx
+++ Coffee-Tracker/src/components/Authentication.jsx
@@ -1,12 +1,33 @@
 import { useState } from "react"
+import { useAuth } from "../context/AuthContext"
 export default function Authentication() {
   const [isRegistration, setIsRegistration]=useState(false)
   const [email , setEmail]=useState('')
   const [password , setPassword]=useState('')
-  const [isauthenticating, setIsAuthenticating]=useState(false)
+  const [isAuthenticating, setIsAuthenticating]=useState(false)
+
+
+  const {signup , login}=useAuth()
 
   async function handleAuthenticate() { 
 
+    if(!email || !email.includes('@') || !password || password.length < 6 || isAuthenticating){
+      return 
+    }
+    try {
+      setIsAuthenticating(true)
+      if(isRegistration){
+
+        await signup(email,password)
+      }else{
+        await login(email,password)
+      }
+      
+    } catch (error) {
+      console.log(error.message)
+    }finally{
+      setIsAuthenticating(false)
+    }
   }
   return (
     <>
@@ -14,7 +35,7 @@ export default function Authentication() {
     <p>{isRegistration? 'Create an account' :'Sign in to your account'}</p>
     <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email'></input>
     <input value={password} onChange={(e) => setPassword(e.target.value)} placeholder='************' type="password" />
-    <button onClick={handleAuthenticate}><p>Submit</p></button>
+    <button onClick={handleAuthenticate}><p>{isAuthenticating? 'Authenticating ...' :'Submit'}</p></button>
     <hr/>
     <div  className='register-content'>
       <p>{isRegistration ? 'Already have an account?' : 'Don\'t have an account?'}</p>
---
+++ Coffee-Tracker/src/context/AuthContext.jsx
@@ -0,0 +1,71 @@
+import { createUserWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { useState, useEffect, useContext , createContext } from 'react';
+import { auth , db } from '../../firebase';
+import { doc , getDoc} from 'firebase/firestore';
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+
+  const [globalUser, setGlobalUser] = useState(null);
+  const [globalState, setGlobalData]=useState(null)
+  const [isLoading, setIsLoading]=useState(false)
+
+  function signup(email, password) { 
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+  function login(email, password) { 
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+  function resetPassword(email){ 
+    return sendPasswordResetEmail(auth, email)
+  }
+  function logout(){
+    setGlobalUser(null)
+    setGlobalData(null)
+    return signOut(auth)
+  }
+  const value = { globalUser , globalState , setGlobalData , isLoading, signup, login, logout, resetPassword }
+
+  useEffect(() => { 
+    const unsubscribe=onAuthStateChanged(auth, async (user)=>{
+      console.log('currentuser',user)
+      if(!user){
+        console.log('No Active User')
+        return}
+      try{
+        setIsLoading(true)
+
+        //first create a reference of document (labled json object)
+        const docRef = doc(db, "users", user.uid)
+        const docSnap = await getDoc(docRef)
+        let firebaseData={}
+        if(docSnap.exists()){
+          console.log('found user data')
+          firebaseData=docSnap.data()
+        }
+        setGlobalData(firebaseData)
+      }
+      catch(error){
+        console.log(error.message)
+      }
+      finally{
+        setIsLoading(false)
+      }
+    })
+    return unsubscribe
+  }, [])
+    
+
+  return(
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
